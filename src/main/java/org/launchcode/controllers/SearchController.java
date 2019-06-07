d51776a253858d976dbecb90b6e2f5c1320226ec
Emma <Emrichardsone@gmail.com>
2019-06-07 15:57:54 -0500

    Please dear lord tell me this meets requirements

--- src/main/java/org/launchcode/controllers/SearchController.java
+++ src/main/java/org/launchcode/controllers/SearchController.java
@@ -4,10 +4,12 @@ import org.launchcode.models.JobData;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+
 
 /**
  * Created by LaunchCode
@@ -22,6 +24,19 @@ public class SearchController {
         return "search";
     }
 
-    // TODO #1 - Create handler to process search request and display results
+//     TODO #1 - Create handler to process search request and display results//
+    @RequestMapping(value="results", method= RequestMethod.GET)
+    public String results (Model model, @RequestParam String searchType, @RequestParam String searchTerm) {
+        model.addAttribute("columns", ListController.columnChoices);
 
+        if (searchType.equals("all")) {
+            ArrayList<HashMap<String, String>> allJobs = JobData.findByValue(searchTerm);
+            model.addAttribute("searchResults", allJobs);
+            return "search";
+        } else {
+            ArrayList<HashMap<String, String>> jobs = JobData.findByColumnAndValue(searchType, searchTerm);
+            model.addAttribute("searchResults", jobs);
+            return "search";
+        }
+    }
 }
